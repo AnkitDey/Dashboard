054ab2b75b8b11536fabdd4ca6495ea7519bd162
AnkitDey <110305620+AnkitDey@users.noreply.github.com>
2023-07-11 15:15:48 +0530

    updated on 11/07/23

---
+++ orders.js
@@ -0,0 +1,34 @@
+const Orders=[
+{
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'pending'
+},
+{
+    productName: 'LARVENDER KF102 Drone',
+    productNumber: '36378',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+},
+{
+    productName: 'Foldable f11 pro Drone',
+    productNumber: '49347',
+    paymentStatus: 'Due',
+    shipping: 'pending'
+},
+{
+    productName: 'Dual camera Mini Drone',
+    productNumber: '93876',
+    paymentStatus: 'paid',
+    shipping: 'Delivered'
+},
+{
+    productName: 'GPS 4k Drone',
+    productNumber: '23156',
+    paymentStatus: 'paid',
+    shipping: 'Delivered'
+},
+
+]
+
